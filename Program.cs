2508401618454336ceef44304f2baaae0813ea15
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-24 03:48:16 +0300

    добавил первую задачу по д/з

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int pow = a;
+
+for (int i = 1; i < b; i++)
+{
+    pow = pow * a;
+}
+Console.WriteLine("A в степени B равно: " + pow);
