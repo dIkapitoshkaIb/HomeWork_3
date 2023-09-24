070540cbf66a5ae188ebed124446c8c8f9341567
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-24 03:55:46 +0300

    добавил вторую задачу по д/з.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-Console.WriteLine("Введите число A: ");
+/* Console.WriteLine("Введите число A: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -10,4 +10,21 @@ for (int i = 1; i < b; i++)
 {
     pow = pow * a;
 }
-Console.WriteLine("A в степени B равно: " + pow);
+Console.WriteLine("A в степени B равно: " + pow); */
+
+
+
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/* Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Сумма всех цифр числа: " + sum); */
